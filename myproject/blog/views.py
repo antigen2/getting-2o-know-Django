be19dec38247b581a28ae146d7c2af1f96f233b6
gib <antigen2@yandex.ru>
2020-03-02 13:13:41 +0400

    До классов

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Обработчик постов. request - объект запроса
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    # # Если отображение без страниц, все на одной
+    # # posts = Post.published.all()
+    # # return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)   # 3 статьи на стр
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница НЕ ЦЕЛОЕ число, возвращаем 1-ую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер стр больше, чем общее кол-во, возвращаем последнюю стр
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 # Обработчик страницы статьи
@@ -14,3 +28,6 @@ def post_detail(request, year, month, day, post):
                              publish__year=year, publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+#
